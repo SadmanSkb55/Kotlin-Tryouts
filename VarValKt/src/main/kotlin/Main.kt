e8a2857836afc05ff09e224787af061d6fbf7a7e
SadmanSkb55 <119743258+SadmanSkb55@users.noreply.github.com>
2023-12-05 15:53:01 +0600

    Var and Val
    gg

---
+++ VarValKt/src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+  var name="Okita"
+  name="Sougo Okita"//var can be changed
+    println(name)
+val name2="sougo"
+ // name2="sougo okita" Val is fixed
+  println(name2)
+ // val name="So"  val cant be var,var cant be val,
+
+  //data types.............................
+  var code: Int=98
+  var code2=90
+  var code3=90.0
+  var code4=9032439472379837.0292378
+  //kotlin will have auto declaration
+  //u can declare int,float,boolean etc too
+  var x=true
+  println(code)
+  println(code2)
+  println(code3)
+  println(code4)
+
+}
