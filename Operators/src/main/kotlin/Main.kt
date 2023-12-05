d5ec5fb17b68bd76a166fbd18d6ad7a72287d30b
SadmanSkb55 <119743258+SadmanSkb55@users.noreply.github.com>
2023-12-05 16:04:47 +0600

    Operators
    gg

---
+++ Operators/src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+   var a=10;
+    var b=20;
+
+    println(a+b)
+    println(a-b)
+    println(a*b)
+    println(a/b)
+    println(a%b)
+    println(a<b)
+    println(a>b)
+    a++
+    b--
+    println(a)
+    println(b)
+ ++a
+ --b
+ println(a)
+ println(b)
+
+}
