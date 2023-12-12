5120020c70f5c9e2140d6cf301c17b316047aa0e
SadmanSkb55 <119743258+SadmanSkb55@users.noreply.github.com>
2023-12-13 02:35:22 +0600

    Kotlin class and object
    KTCO

---
+++ KTCO/src/main/kotlin/Classofkt.kt
@@ -0,0 +1,4 @@
+class Classofkt {
+    var name:String?=null
+    val name2:String="GG"
+}
---
+++ KTCO/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+var x=Classofkt()
+    println("Enter name")
+    x.name= readln().toString()
+   // val y=Classofkt() impossible
+    println("${x.name}")
+    println("${x.name2}")
+
+    var y=ClassofJv();
+    y.name="Argon"
+  //  ClassofJv z=new ClassofJv(); impossible
+    println("${y.name}")
+}
